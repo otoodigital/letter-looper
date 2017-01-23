f7f802eb55a1cd36bd62af9b32c906eb3fb75328
Kwesi Otoo <otoo0013@algonquinlive.com>
2017-01-23 12:54:44 -0500

    Create looper.js

---
+++ js/looper.js
@@ -0,0 +1,16 @@
+var letter;
+var i;
+var k;
+/*var i = 0
+var k = 0*/
+
+while (!letter) {
+    letter = prompt("Enter a character");
+    for (i = 0; i < 10; i += 1) {
+   document.write("<br>");
+    for (k = 0; k < i; k += 1) {
+   document.write(letter)
+        
+} 
+} 
+}
