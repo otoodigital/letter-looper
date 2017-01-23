c7dec9956b56f1a566e52a45b8f180a2c404a256
Kwesi Otoo <otoo0013@algonquinlive.com>
2017-01-23 13:34:50 -0500

    updated formatting

--- js/looper.js
+++ js/looper.js
@@ -6,9 +6,10 @@ var k = 0*/
 
 while (!letter) {
     letter = prompt("Enter a character");
-    for (i = 0; i < 10; i += 1) {
+
+for (i = 0; i < 10; i += 1) {
    document.write("<br>");
-    for (k = 0; k < i; k += 1) {
+for (k = 0; k < i; k += 1) {
    document.write(letter)
         
 } 
